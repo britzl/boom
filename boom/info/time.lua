01e095650971f93345ffce4b6d85dc072f07dfaf
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-05-09 10:41:00 +0200

    Added fps()

--- boom/info/time.lua
+++ boom/info/time.lua
@@ -2,8 +2,9 @@ local M = {}
 
 
 local dt = 0
+local prev_dt = 0
 local time = 0
-
+local fps = 60
 ---
 -- Get the delta time
 -- @return dt Delta time
@@ -18,12 +19,20 @@ function M.time()
 	return socket.gettime() - timew
 end
 
+function M.fps()
+	return fps
+end
+
 function M.__init()
 	time = socket.gettime()
 end
 
 function M.__update(_dt)
+	prev_dt = dt
 	dt = _dt
+
+	local smoothing = math.pow(0.9, dt * 60 / 1000);
+	fps = (fps * smoothing) + (1 / dt * (1.0 - smoothing))
 end
 
 

3278ca51a6ab799f896db7ebd50737dd90b1e727
Björn Ritzl <bjorn.ritzl@gmail.com>
2023-05-09 22:14:54 +0200

    Update gravity.lua

--- boom/info/gravity.lua
+++ boom/info/gravity.lua
@@ -2,16 +2,14 @@ local M = {}
 
 local gravity = 800
 
----
--- Get gravity
--- @return gravity The gravity in pixels per seconds
+--- Get gravity
+-- @treturn string gravity The gravity in pixels per seconds
 function M.get_gravity()
 	return gravity
 end
 
----
--- Set gravity
--- @param gravity Gravity in pixels per seconds
+--- Set gravity
+-- @number gravity Gravity in pixels per seconds
 function M.set_gravity(_gravity)
 	gravity = _gravity
 end
